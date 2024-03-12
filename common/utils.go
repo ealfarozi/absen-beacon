4a863f1751928ece6d01f471d1f78ad931999ecb
Erlangga Al Farozi <erlangga.al.farozi@gmail.com>
2024-03-12 11:11:31 +0700

    no message

--- common/utils.go
+++ common/utils.go
@@ -73,7 +73,7 @@ func SetHash(str string) uint64 {
 }
 
 func GetHash() {
-	HASHED = strconv.FormatUint(SetHash(UUID+":"+GetUUID()), 12)
+	HASHED = strconv.FormatUint(SetHash(UUID+":"+GetUUID()), 15)
 }
 
 func GetVars() {
